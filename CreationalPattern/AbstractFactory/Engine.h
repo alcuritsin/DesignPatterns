9b93f470850afb2165cdd1de5115e9c3923ff245
alcuritsin <al.curitsin@gmail.com>
2021-08-13 21:10:52 +0500

    AbstractFactory --done

---
+++ CreationalPattern/AbstractFactory/Engine.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Engine
+{
+protected:
+	unsigned int power;
+public:
+	Engine(unsigned int power) :power(power) { }
+	virtual ~Engine() { }
+
+	virtual unsigned int get_power() const = 0;
+	void info()const
+	{
+		cout << typeid(*this).name() << ";   power: " << power << " hors\n";
+	}
+};
+
+class SportEngine:public Engine
+{
+public:
+	SportEngine(): Engine(500) { }
+	~SportEngine() { }
+
+	unsigned int get_power()const
+	{
+		return this->power;
+	}
+};
+
+class TruckEngine :public Engine
+{
+public:
+	TruckEngine() :Engine(1000) { }
+	~TruckEngine() { }
+
+	unsigned int get_power()const
+	{
+		return this->power;
+	}
+};
